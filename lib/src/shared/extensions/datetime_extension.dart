20b30352f77c1880f52fe14a67af5d29781e29d9
Erbay MAVZER <mavzerbay@gmail.com>
2024-08-27 15:05:27 +0300

    feat: Improve calculation of day difference in DateTime extension
    Refactor the `getDay` method in the `TimeDifference` extension of the `DateTime` class to improve the calculation of the day difference. Instead of directly comparing the difference in days between the current date and the given date, the method now compares the difference between the current date and the given date at midnight. This ensures accurate calculation of the day difference, especially when dealing with time zones and daylight saving time changes.
    
    Modified file:
    - lib/src/shared/extensions/datetime_extension.dart

--- lib/src/shared/extensions/datetime_extension.dart
+++ lib/src/shared/extensions/datetime_extension.dart
@@ -2,7 +2,11 @@ import 'package:intl/intl.dart';
 
 extension TimeDifference on DateTime {
   String getDay({String chatSeparatorDatePattern = 'dd MMM yyyy'}) {
-    final differenceInDays = difference(DateTime.now()).inDays;
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day);
+    final dateToCompare = DateTime(year, month, day);
+
+    final differenceInDays = dateToCompare.difference(today).inDays;
     if (differenceInDays == 0) {
       return 'Today';
     } else if (differenceInDays <= 1 && differenceInDays >= -1) {
